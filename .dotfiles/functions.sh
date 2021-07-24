4b79966073827c788806a0d1c90e904fba337801
Steven Kalt <kalt.steven@gmail.com>
2021-07-24 12:43:24 -0400

    fix(functions/til): only `git add` and push a til-file if it changed

--- .dotfiles/functions.sh
+++ .dotfiles/functions.sh
@@ -1,36 +1,49 @@
 #!/usr/bin/env bash
 
 til() {
-    local target; target="$HOME/.til/$(date +'%Y/%m/%d').md"
-    local target_dir="${target%/*}";
+    local target
+    target="$HOME/.til/$(date +'%Y/%m/%d').md"
+    local target_dir="${target%/*}"
     mkdir -p "$target_dir"
     (
-        cd "$target_dir" || exit 127;
-        local amend="";
+        cd "$target_dir" || exit 127
+
+        # ensure the git directory is configured
         if ! (git rev-parse); then
             (
                 cd "$HOME/.til" &&
-                git init &&
-                git remote add origin https://github.com/SKalt/til.git &&
-                git fetch &&
-                git checkout --track origin/master
-            );
+                    git init &&
+                    git remote add origin https://github.com/SKalt/til.git &&
+                    git fetch &&
+                    git checkout --track origin/master
+            )
         else
             git pull
         fi
+
+        # ensure a preformatted file is at $target
         if ! test -e "$target"; then
-            amend="false"
             {
                 echo "---"
                 echo "tags: []"
                 echo "---"
-            } > "$target"
-        else amend="true"
+            } >"$target"
         fi
+
+        local before
+        before="$(shasum -a 256 "$target")"
         ${EDITOR:-vi} "$target"
-        git add "$target"
-        if [ "$amend" = "true" ]; then git commit --no-gpg-sign --amend;
-        else git commit --no-gpg-sign -m "til";
+        local after
+        after="$(shasum -a 256 "$target")"
+
+        if [ "$before" != "$after" ]; then
+            git add "$target"
+            if (git status --short | grep -qe '^.M'); then
+                git commit --no-gpg-sign --amend
+            else
+                git commit --no-gpg-sign -m "til"
+            fi
+            git push
         fi
     )
 }
