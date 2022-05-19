3526ede16bf08a1fabf0bacfdcfc24c2f5cf027a
Steven Kalt <kalt.steven@gmail.com>
2022-05-19 10:12:30 -0400

    feat(fns): split out tailscale ip lookup function, pass username to into

--- .dotfiles/functions.sh
+++ .dotfiles/functions.sh
@@ -31,14 +31,31 @@ alert() {
     fi
 }
 
+tailscale_ip() {
+    ### tailscale_ip [QUERY]
+    ### search tailscale status by node name
+    if [ -n "${1:-}" ]; then
+        tailscale status | grep "$1" | awk '{ print $1 }'
+    else
+        tailscale status |    fzf    | awk '{ print $1 }'
+    fi
+}
+
 into() {
     local target_ip
-    if [ -n "${1:-}" ]; then
-        target_ip="$(tailscale status | grep "$1" | awk '{ print $1 }')"
-    else
-        target_ip="$(tailscale status |    fzf    | awk '{ print $1 }')"
-    fi
-    ssh "$target_ip"
+    local username;
+    local query;
+    while [ -n "${1:-}" ]; do
+      case "$1" in
+        -h|--help) echo "into [-h|--help] [[-u] USER | [--user[=]]USER] [QUERY]" && exit 0;;
+        -u|--user) shift; username=$1; shift; ;;
+        --user=*) username="${1%=*}"; shift;;
+        *) query="$1"; shift;;
+      esac
+    done
+    if test -z "$username"; then username="$(whoami)"; fi
+    target_ip="$(tailscale_ip "$query")"
+    ssh "${username}@${target_ip}"
 }
 
 pretty_path() { echo "$PATH" | tr ':' '\n'; }
