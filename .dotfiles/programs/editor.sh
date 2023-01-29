38d444bf4556246a11331b03dc743c5749d78d69
Steven Kalt <kalt.steven@gmail.com>
2023-01-29 10:18:25 -0500

    feat: check for preferred editors

--- .dotfiles/programs/editor.sh
+++ .dotfiles/programs/editor.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-EDITOR='vi'
-if command -v code &>/dev/null; then EDITOR='code --wait'; fi;
+is_installed() { command -v "$1" &>/dev/null; };
+if   is_installed hx;   then EDITOR='hx';
+elif is_installed code; then EDITOR='code --wait';
+elif is_installed nvim; then EDITOR='nvim'
+elif is_installed vim;  then EDITOR='vim'
+elif is_installed vi;   then EDITOR='vi';
+elif is_installed nano; then EDITOR='nano';
+else EDITOR='ed'; # <- part of posix
+fi;
 export EDITOR;
