e861062c671c57dbc262304cfb476dcd9a89dcc1
Steven Kalt <kalt.steven@gmail.com>
2024-11-10 19:01:15 -0500

    feat: add setup script for GH codespaces

--- .dotfiles/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-git clone --bare https://github.com/SKalt/dotfiles.git "$HOME/.dotfiles.git"
-alias dotfiles="git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME"
-dotfiles config core.excludesFile "$HOME/.dotfiles/dotfiles_exclude"
-dotfiles checkout
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# specifically for https://docs.github.com/en/codespaces/setting-your-user-preferences/personalizing-github-codespaces-for-your-account#dotfiles
+# shellcheck disable=SC2296
+set -eux
+
+# assuming the current working directory is the dotfiles repo...
+for f in $(git ls-files); do
+  if [ -f "$HOME/$f" ]; then
+    echo "[INFO] File already exists: $HOME/$f"
+    mv "$HOME/$f" "$HOME/$f.bak"
+  fi
+done
+rm -rf "$HOME/.dotfiles.git"
+cp -r .git "$HOME/.dotfiles.git"
+
+dotfiles() { git --git-dir="$HOME/.dotfiles.git/" --work-tree="$HOME" "$@"; }
+dotfiles config core.excludesFile "$HOME/.dotfiles/dotfiles_exclude"
+dotfiles checkout
+dotfiles reset --hard
