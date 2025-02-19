29a33ae107c37966bd2a4737b8f63b46e00d8984
Steven Kalt <kalt.steven@gmail.com>
2025-02-19 13:11:06 -0500

    refactor: rephrase `dotfiles` alias using GIT_DIR/WORK_TREE rather than flags
    Hypothetically, this might help with git subcommands, but IDK.

--- .dotfiles/aliases.sh
+++ .dotfiles/aliases.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # shellcheck disable=SC2139
-alias dotfiles="git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+alias dotfiles="GIT_DIR=$HOME/.dotfiles.git/ GIT_WORK_TREE=$HOME git"
