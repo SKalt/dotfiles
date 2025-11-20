b6b3afd89fbf7432b5ef6fb56363fdf38d433328
Steven Kalt <kalt.steven@gmail.com>
2025-11-20 10:09:50 -0500

    feat: experiment with (neo)vim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,13 @@
+
+-- https://nvim-mini.org/mini.nvim/readmes/mini-snippets.html#quickstart
+local gen_loader = require('mini.snippets').gen_loader
+require('mini.snippets').setup({
+  snippets = {
+    -- Load custom file with global snippets first (adjust for Windows)
+    gen_loader.from_file('~/.config/nvim/snippets/global.json'),
+
+    -- Load snippets based on current language by reading files from
+    -- "snippets/" subdirectories from 'runtimepath' directories.
+    gen_loader.from_lang(),
+  },
+})
---
+++ .vimrc
@@ -0,0 +1,7 @@
+call plug#begin()
+Plug 'nvim-mini/mini.nvim'
+call plug#end()
+
+if has('nvim')
+  "nvim stuff goes here
+endif
