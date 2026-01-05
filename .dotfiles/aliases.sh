d8e405c503ea8b2bfc2890355ebdd956e416b2bd
Steven Kalt <kalt.steven@gmail.com>
2026-01-05 12:42:21 -0500

    feat: alias cat->bat when bat is present

--- .dotfiles/aliases.sh
+++ .dotfiles/aliases.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # shellcheck disable=SC2139
 alias dotfiles="git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+alias ,wd='cd "$(git worktree-dir)"'
+if command -v bat >/dev/null; then alias cat=bat; fi
