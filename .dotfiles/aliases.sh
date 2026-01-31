a920d12eb55144019ea1e85c35662dc62fa1b884
Steven Kalt <kalt.steven@gmail.com>
2026-01-31 08:44:29 -0500

    feat: add navigation aliases for project directories

--- .dotfiles/aliases.sh
+++ .dotfiles/aliases.sh
@@ -3,3 +3,23 @@
 alias dotfiles="git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
 alias ,wd='cd "$(git worktree-dir)"'
 if command -v bat >/dev/null; then alias cat=bat; fi
+# shellcheck disable=SC2142,2154
+alias ,w='w="$(
+  find ~/work  -type d -maxdepth 3 -name "*.git"  -exec git --git-dir={} worktree  list ";" |
+    sed "s#$HOME/work/##g" |
+    awk "{ print \$1 }" |
+    sort -u |
+    fzf |
+    sed "s#^#$HOME/work/#g"
+)"; cd "${w:-.}"; unset w
+'
+# shellcheck disable=SC2142,2154
+alias ,p='p=$(
+  find ~/programming ~/programming/forks  -type d -maxdepth 3 -name "*.git"  -exec git --git-dir={} worktree  list ";" |
+    sed "s#$HOME/programming/##g" |
+    awk "{ print \$1 }" |
+    sort -u |
+    fzf |
+    sed "s#^#$HOME/programming/#g"
+); cd "${p:-.}"; unset p
+'
