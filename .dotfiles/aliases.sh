8f029a03f7fe20238f1c54b65a61a8fb88a8dbbc
Steven Kalt <kalt.steven@gmail.com>
2026-04-06 17:10:00 -0400

    feat: split worktree-picker into own script

--- .dotfiles/aliases.sh
+++ .dotfiles/aliases.sh
@@ -4,24 +4,8 @@ alias dotfiles="git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
 alias ,wd='cd "$(git worktree-dir)"'
 if command -v bat >/dev/null; then alias cat=bat; fi
 # shellcheck disable=SC2142,2154
-alias ,w='w="$(
-  find ~/work -maxdepth 3 -type d -name "*.git"  -exec git --git-dir={} worktree  list ";" |
-    sed "s#$HOME/work/##g" |
-    awk "{ print \$1 }" |
-    sort -u |
-    fzf |
-    sed "s#^#$HOME/work/#g"
-)"; cd "${w:-.}"; unset w
-'
+alias ,w='w="$( ~/bin/git-pick-worktree ~/work )"; cd "${w:-.}"; unset w'
 # shellcheck disable=SC2142,2154
-alias ,p='p=$(
-  find ~/programming ~/programming/forks -maxdepth 3 -type d -name "*.git"  -exec git --git-dir={} worktree  list ";" |
-    sed "s#$HOME/programming/##g" |
-    awk "{ print \$1 }" |
-    sort -u |
-    fzf |
-    sed "s#^#$HOME/programming/#g"
-); cd "${p:-.}"; unset p
-'
+alias ,p='p=$( ~/bin/git-pick-worktree ~/programming ); cd "${p:-.}"; unset p'
 # shellcheck disable=SC2142,2154
 alias tt='tt=; tt=$(~/bin/tt) && cd "${tt:-.}"; unset tt'
